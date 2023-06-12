7f337f67620251139768cbbaad0317bc9cc828b6
Brandon-M-Williams <brandonmwilliams34@gmail.com>
2023-06-11 18:51:19 -0700

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The Latest in Canon Photography Gear</title>
+    <link rel="stylesheet" type="text/css" href="screen.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <!-- Your HTML content here -->
+</body>
+</html>
