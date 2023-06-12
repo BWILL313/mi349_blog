157c99d65cd558cdb9451cfa0025e3f300d7b969
Brandon-M-Williams <brandonmwilliams34@gmail.com>
2023-06-11 19:37:19 -0700

    commit

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// JavaScript code goes here
 
-
-// Example code to log a message to the console
 console.log("Hello, JavaScript!");
 
-// Example code to change the background color of the body
 document.body.style.backgroundColor = "#F6F6F6";
